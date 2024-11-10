e708e01e2435b9178651977ae422928d7a8b6a76
akv3sic <antekvesic2@gmail.com>
2024-11-10 15:59:12 +0100

    Enhance app layout with text alignment and padding adjustments

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-center">
     <img src="/firesync.png" alt="Firesync Logo" class="w-24 h-24 mb-4" />
 
     <h1 class="text-3xl font-bold text-gray-700">FireSync</h1>
 
-    <p class="text-lg text-gray-700">Učinkovito upravljanje intervencijama i resursima za vatrogasce</p>
+    <p class="text-lg text-gray-700 px-4">Učinkovito upravljanje intervencijama i resursima za vatrogasce</p>
   </div>
 </template>
 
