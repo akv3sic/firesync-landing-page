d201e7048fb385977558e3ed898cba1e0e59611a
akv3sic <antekvesic2@gmail.com>
2024-11-10 15:09:43 +0100

    Add simple app layout with logo and title

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <img src="/firesync.png" alt="Firesync Logo" class="w-24 h-24 mb-4" />
+
+    <h1 class="text-3xl font-bold text-gray-700">FireSync</h1>
+
+    <p class="text-lg text-gray-700">Učinkovito upravljanje intervencijama i resursima za vatrogasce</p>
   </div>
 </template>
